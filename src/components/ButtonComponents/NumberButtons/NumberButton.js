bd37bb99d1609051bd261f92c162a8a36bd36304
DentonWarnock <dentonwarnock@gmail.com>
2019-09-19 22:00:20 -0700

    Numbers data passed all the way up and numbers buttons now display on the screen.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const NumberButton = () => {
+export default function NumberButton(props) {
   return (
     <>
+      <button key={props.key}>{props.num}</button> 
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </>
   );
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { numbers } from "../../../data.js";
+import NumberButton from "./NumberButton.js";
 
 
-const Numbers = () => {
+export default function Numbers() {
   const [numberState, setNumberState] = useState(numbers);
 
   return (
     <div>
-
+      {numberState.map((num, index) => (
+        <NumberButton key={index} num={num} />
+      ))}
+      
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
